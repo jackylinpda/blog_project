b139392eaa4e12c73c5bc8ebb455300d817c639b
jack lin <linjiafa@gmail.com>
2018-03-13 23:17:26 +0800

    首次提交Django项目文件

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -74,16 +74,6 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-'''
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -132,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT  = os.path.join(BASE_DIR, "blog/static")
